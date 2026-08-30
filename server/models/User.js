f87529e9616256abf647cd90efa60d30b280e4de
John <johnjerry8749@gmail.com>
2026-08-30 22:37:21 +0100

    save

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,18 @@
-import express from "express";
 import pool from "../config/db";
+
+
+export const findUserByEmail = async (email) =>{
+    const result = await pool.query(
+        "SELECT * FROM users WHERE email=$1",
+        [email]
+    );
+    return result.rows[0];
+};
+
 
 export const Registeruser = async (name, email, password) => {
   const result = await pool.query(
-    "INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *",
+    `INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email`,
     [name, email, password],
   );
   return result.rows[0];
