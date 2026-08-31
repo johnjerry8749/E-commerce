8a3dd56d97d79bab89bbc775636e7e4189239dec
John <johnjerry8749@gmail.com>
2026-08-31 01:57:41 +0100

    Fix PostgreSQL connection and authentication :wq#

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,43 @@
-import { Registeruser } from "../models/User";
-import { findUserByEmail } from "../models/User";
-import { hashPasswod } from "../utils/hashPassword";
-import { generateToken } from "../utils/generateToken";
+import { Registeruser, findUserByEmail } from "../models/User.js";
+import { hashPasswod } from "../utils/hashPassword.js";
+import { generateToken } from "../utils/generateToken.js";
+import { validationResult } from "express-validator";
 
 export const register = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    //Check Validation erros
+    const erorrs = validationResult(req);
+    if (!erorrs.isEmpty()) {
+      return res.status(400).json({
+        sucsess: false,
+        error: erorrs.array(),
+      });
     }
-}
+    const { name, email, password } = req.body;
+
+    //Ceck if user don exist
+    const existinguser = await findUserByEmail(email);
+    if (existinguser){
+        return res.status(400).json({
+            success:false,
+            Message: "Email Already Exist"
+        });
+    }
+
+        //Hash Password
+        const hashedPassword = await hashPasswod(password);
+
+        //Create user in Database
+        const newUser = await Registeruser({name, email, password});
+
+        const token = await generateToken(newUser.id);
+
+        //send response
+        res.status(201).json({
+            success: true,
+            Message: "Registered Successfully",
+            user:{
+        }});
+
+  } catch (error) {}
+};
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import authRouter from "./authRoutes.js";
+
+const router = Router();
+
+router.use("/auth", authRouter);
+
+
+export default router
